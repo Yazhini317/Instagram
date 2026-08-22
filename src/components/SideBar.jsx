653a1b9a36939f9e5a26e41ecfa986aff4d99b34
maaran351 <maaran351@gmail.com>
2026-08-23 00:53:49 +0530

    Updated

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -71,7 +71,7 @@ function SideBar({ user }) {
 
           {/* middle*/}
 
-          <Nav className="nav flex-column justify-content-evenly 100vh">
+          <Nav className="nav flex-column justify-content-evenly">
             <Nav.Link
               className="text-dark"
               onClick={() => {
@@ -192,7 +192,14 @@ function SideBar({ user }) {
                 </>
               }
               className="sidebar-dropdown  fs-6 d-flex  text-dark align-items-center "
-              
+                popperConfig={{
+    modifiers: [
+      {
+        name: "flip",
+        enabled: false,
+      },
+    ],
+  }}
     
            >
               
