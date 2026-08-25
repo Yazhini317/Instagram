8e45b81a4c647bab934ad4da7a24f7d1a53feedf
maaran351 <maaran351@gmail.com>
2026-08-25 20:11:52 +0530

    Updated

--- src/components/SavedPost.jsx
+++ src/components/SavedPost.jsx
@@ -68,7 +68,7 @@ const SavedPost = () => {
            
                         <div
                           key={saved.id}
-                          className="d-flex flex-column align-items-start p-3 ms-3 gap-2 postContainer"
+                          className=" postContainer"
                         >
                           <div className="d-flex post gap-2 align-items-center">
                             <img 
