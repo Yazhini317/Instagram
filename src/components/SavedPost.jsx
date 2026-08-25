1a630dd2fd7e0e68e3002cb43985416d9c053f50
maaran351 <maaran351@gmail.com>
2026-08-25 19:58:19 +0530

    Updated again

--- src/components/SavedPost.jsx
+++ src/components/SavedPost.jsx
@@ -68,10 +68,10 @@ const SavedPost = () => {
            
                         <div
                           key={saved.id}
-                          className="d-flex  align-items-start p-3 ms-3 gap-2 postContainer"
+                          className="d-flex flex-column align-items-start p-3 ms-3 gap-2 postContainer"
                         >
                           <div className="d-flex post gap-2 align-items-center">
-                            <img
+                            <img 
                               src={saved.user.profileImage}
                               className="rounded-circle"
                               alt=""
